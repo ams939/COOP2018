4e675d8ce42d8f0e17d0d106139f8e46ed24c13c
ams939 <ams939@drexel.edu>
2018-05-24 11:30:35 -0400

    Fixed DBQuery return format

--- APIPrototype/DataWorkflow/main.py
+++ APIPrototype/DataWorkflow/main.py
@@ -13,19 +13,19 @@ def main():
         }
     
     limit = 1
-    uuid = "053fdcd5-236a-4a1a-9d3e-0b938b1e93f1"
+    uuid = "816a6cef-e8c6-4606-b57f-1bf17294e29b"
     
-    table_name = "records"
+    table_name = "records1"
     
     #Condcut a query in the database using search terms in rq
-    records_query = searchRecords(rq, table_name, limit)
-    print(records_query)
+    #records_query = searchRecords(rq, table_name, limit)
+    #print(records_query)
     
     print("\n\n\n")
     
     #Search for a record in the database
-    #record = viewRecord(uuid, table_name)
-    #print(record)
+    record = viewRecord(uuid, table_name)
+    print(record)
     
 if __name__ == "__main__":
     main()
